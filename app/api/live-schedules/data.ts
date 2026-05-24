915eb96f8d8e3afe1b88b41f0080b43b34def963
Shohei Tanahara <shoheitanahara@Shoheis-MacBook-Air.local>
2026-05-24 11:03:59 +0900

    update live schedule

--- app/api/live-schedules/data.ts
+++ app/api/live-schedules/data.ts
@@ -232,6 +232,548 @@ DOOR:	未定
     date: '2025-12-06',
     image: '/images/2025-12-06.jpg', 
   },
+  {
+    title: 'Hard Hit Psycho Illusion',
+    description: `
+      2025.3.21 sat
+      CLUB HEAVY SICK
+      OPEN & START 17:30
+      Adv ¥2,800+1d / Door ¥3,300+1d
+
+      BAND
+      STOMPIN' RIFFRAFFS
+      CHICKEN JUMP SKIPS
+      Hi-NOMADY
+      STREETCATS
+      Tamwind
+
+      DJ
+      ますみ（DRY BONES）
+      Kartooze
+      IKB SKATE PSYCHOS（D.T.F.S!!! Records）
+      OVER ALL 7
+    `,
+    date: '2025-03-21',
+    image: '/images/2025-03-21.png',
+  },
+  {
+    title: 'From Australia The Koffin Rockers Japan Tour 2025',
+    description: `
+      2025.10.3 fri
+      Live & Pub MOON STEP
+      Door open 18:30 / Show start 19:00
+      ticket 当日 2900 yen / pre-order 2400 yen / +drink charge 600 yen
+
+      The Koffin Rockers
+      SPANISH BARROWIN' GUITAR
+      Chicken Jump Skips
+      Hi-Nomady
+      爆裂ジャイロ
+
+      DJ MEGLO
+    `,
+    date: '2025-10-03',
+    image: '/images/2025-10-03.png',
+  },
+  {
+    title: 'DUB CONNECTION',
+    description: `
+      2025.11.3
+      WALL LIVE HOUSE
+      OPEN 17:00 / SHOW TIME 17:45
+      DOOR ¥2000 + 1 DRINK
+
+      Hi-Nomady
+      CHARRA PANKS
+      Ninja wi Ninja
+      SUPER CLASS
+      天狗ROCKERS
+
+      DJ
+      masa & mittuu（militant ites）
+      Yujin Curtis
+    `,
+    date: '2025-11-03',
+    image: '/images/2025-11-03.png',
+  },
+  {
+    title: 'Hard Hit Psycho Illusion',
+    description: `
+      2025.11.8 sat
+      CLUB HEAVY SICK
+      Ticket adv ¥2800+1D / door ¥3300+1D
+
+      BAND
+      The Psyclocks
+      Villainz
+      Hi-Nomady
+      BugDuckTux（Fukushima）
+      Mitchy Dead with Garbageman
+
+      DJs
+      TAMAE（Ponytailz）
+      MIRI（西成帝国）
+      Ikb Skate Psychos（D.T.F.S!!! Records）
+    `,
+    date: '2025-11-08',
+    image: '/images/2025-11-08.png',
+  },
+  {
+    title: '7inch EP 発売 ONLY 100',
+    description: `
+      2025.11.23
+      長野 the venue
+      op / 6PM
+      料金 ¥4000（1杯 or 1枚付き）
+
+      コゴローズ
+      ぱっち
+      and young...
+      Mr.Fin & Jake
+      前川エクスプロージョン
+      Hi-NOMADY
+
+      DJ OUTSIDER
+    `,
+    date: '2025-11-23',
+    image: '/images/2025-11-23.png',
+  },
+  {
+    title: 'P SYCHOOO LIVE',
+    description: `
+      2025.12.13 sat
+      Benten Music Club
+      20:00 OPEN
+      2000yen 1d
+
+      JERICHO BUNCH（北九州）
+      Hi-NOMADY（東京）
+      R.SK & HIDA NAMU（福島 千葉）
+
+      DJ
+      Co? HAJIME
+      TETSU
+      SHIGI
+
+      Live Paint
+      Muku Rozi
+    `,
+    date: '2025-12-13',
+    image: '/images/2025-12-13.png',
+  },
+  {
+    title: 'Back From The Grave Returns Monster Christmas Party',
+    description: `
+      2025.12.20 sat
+      UFO CLUB
+      OPEN: TBA
+
+      TEXACO LEATHERMAN
+      SATURNS
+      BLACK AND WHITE
+      STOMPIN' RIFFRAFFS
+      THE FADEAWAYS
+      BOBBY'S BAR
+      THE BOING
+      and more...
+
+      DJ BFTG
+    `,
+    date: '2025-12-20',
+    image: '/images/2025-12-20.png',
+  },
+  {
+    title: 'PSYCHOHOLIC REVOLT',
+    description: `
+      2025.12.29
+      SHINJUKU LOFT
+      OPEN 14:00 / START 14:30
+      TICKET ADV ¥4000 / DOOR ¥4500
+    `,
+    date: '2025-12-29',
+    image: '/images/2025-12-29.png',
+  },
+  {
+    title: 'DIEYZ Night',
+    description: `
+      2026.1.24 sat
+      Shinjuku HEIST
+      OPEN 17:00 / START 17:30
+      前売 ¥2500 / 当日 ¥3000（1DRINK別）
+
+      Hi-Nomady
+      ALL FOR MEAL
+      Feetrajec
+      KEBAB
+      NEATZ
+      Peppermint stars
+      浅草銀蠅
+    `,
+    date: '2026-01-24',
+    image: '/images/2026-01-24.png',
+  },
+  {
+    title: 'Heavy Sick Strange Carnival',
+    description: `
+      2026.1.25 sunday
+      club HEAVY SICK
+      Open 18:00 / Start 18:30
+      ADV ¥2700+1d / DOOR ¥3000+1d
+
+      ラブホテル
+      CABARETS
+      Hi-Nomady
+      VAMPIRE BATS
+      PEPPERMINT STARS
+
+      DJ
+      Robinson
+      Ikb skate Psychos（D.T.F.S. Records）
+    `,
+    date: '2026-01-25',
+    image: '/images/2026-01-25.png',
+  },
+  {
+    title: 'PSYCHO SIDE SHOW vol.11',
+    description: `
+      2026.2.7 sat
+      ISE BARRET
+      Open / Start 18:00
+      Ticket 2500
+
+      HEAVEN'S GATE
+      HYDRO STOMPERS
+      HI-NOMADY
+      FREE CHARGE
+
+      DJ
+      GINTOKI
+      NAOTO
+    `,
+    date: '2026-02-07',
+    image: '/images/2026-02-07.png',
+  },
+  {
+    title: 'RIDE-OUT MEETING',
+    description: `
+      2026.3.7 sat
+      柴崎ラットホール
+      open 18:00 / start 18:30
+      ADV ¥2,500+1d / DOOR ¥3,000+1d
+
+      Falling Sugar
+      The Bit
+      Hi-NOMADY
+      HYDRO STOMPERS
+
+      DJミドリ
+    `,
+    date: '2026-03-07',
+    image: '/images/2026-03-07.png',
+  },
+  {
+    title: 'MR.DEATH TRIBUTE 1965-2025',
+    description: `
+      2026.3.15 sun
+      HEAVY SICK
+      OPEN 12:00
+      TICKET 2500円+1D / TEEN 1500円+1D
+
+      BFTG R&B BASTARDS
+      CULT DIGGIN'
+      N.P.H.
+      LONEY HOP
+      THE VIRUS
+      NERVOUS FREAK
+      TWISTIN' RUMBLE
+      BAND A-Z
+      ANGEL FACE
+      BOBBY'S BAR
+      CHICKEN JUMP SKIPS
+      HI-NOMADY
+      JERICHO BUNCH
+      JET BOYS
+      K.S. HARD BAND
+    `,
+    date: '2026-03-15',
+    image: '/images/2026-03-15.png',
+  },
+  {
+    title: 'DUB CONNECTION',
+    description: `
+      2026.4.5 sun
+      WALL LIVE HOUSE
+      open 17:00 / show time 17:45
+      Door ¥2500 + 1drink
+
+      Hi-Nomady
+      ZEROZONE
+      Ninja wi Ninja
+      LOS MEQUILA COKES
+      天狗ROCKERS
+
+      DJ
+      masa & mittuu（militant ites）
+      SOUND NOBU
+    `,
+    date: '2026-04-05',
+    image: '/images/2026-04-05.png',
+  },
+  {
+    title: 'Sickabilly Night',
+    description: `
+      2026.4.11 sat
+      CLUB HEAVY SICK
+      OPEN 18:00 / START 18:30
+      ADV ¥2800+1D / DOOR ¥3300+1D
+
+      Jericho Bunch
+      Hi-NOMADY
+      The Psyclocks
+      Souta and the Blitz Attack Boys
+      Mitchy Dead
+
+      DJs
+      Ikb Skate Psychos（D.T.F.S!!! Records）
+      MOMO（Stormy）
+
+      ARTWORK BY KARTOOZE
+    `,
+    date: '2026-04-11',
+    image: '/images/2026-04-11.png',
+  },
+  {
+    title: 'PSYCHO TROPICAL',
+    description: `
+      2026.4.12 sun
+      Open / Start DJs 19:00 / Close 23:00
+      Tickets: ¥2000 + 1 Drink Order
+
+      LIVE
+      LOS MEQUILA COKES × Hi-NOMADY
+
+      DJ
+      AmemiyaKSK
+      TOSSY
+      TACOS
+      BAILEM TACOS
+      SHOP
+      MEZKOSMICA
+    `,
+    date: '2026-04-12',
+    image: '/images/2026-04-12.png',
+  },
+  {
+    title: 'BEAT STORM FES. Vol.1',
+    description: `
+      2026.4.19 sun
+      大塚MEETS
+      OPEN 12:15 / START 12:30
+      前売 ¥2,500 / 当日 ¥3,000（ドリンク代別）
+
+      The Naughty Dogs
+      DRAGDOWN
+      前西原夕子
+      Hi-NOMADY
+    `,
+    date: '2026-04-19',
+    image: '/images/2026-04-19.png',
+  },
+  {
+    title: 'HILLBILLY KATS JAMBOREE vol.16',
+    description: `
+      2026.4.26 sun
+      柏 Animalia
+      OPEN 17:55 / START 18:10
+      予約 3,300円 / 当日 3,800円（+2ドリンクオーダー）
+
+      ハッチハッチェル
+      蜂と瓜
+      Hi-NOMADY
+      PEPPERMINT STARS
+    `,
+    date: '2026-04-26',
+    image: '/images/2026-04-26.png',
+  },
+  {
+    title: 'NAKANO MOONSTEP IN DA HOUSE',
+    description: `
+      2026.5.2 sat
+      NAKANO MOONSTEP
+      OPEN 16:00
+      ¥2000 1d Inc
+
+      LIVE
+      前川エクスプロージョン
+      有刺鉄線
+      ベラー
+      P-iPLE
+      TORO
+      Y×A×D
+      Hi-NOMADY
+      the dust.
+      The Wedgies
+      GANDH-X
+
+      DJs
+      Daddy-O-Nov
+      buddy-ito
+      Bisco
+      Suzu（The Hightmarts）
+      Naoya（Bobby's Bar）
+      Suga-Jun（The Wedgies）
+    `,
+    date: '2026-05-02',
+    image: '/images/2026-05-02.png',
+  },
+  {
+    title: 'BIG RUMBLE TOKYO 2026',
+    description: `
+      2026.5.3 sun
+      SHINJUKU LOFT
+      OPEN 11:30 / START 12:00
+      ADV ¥5000 / DOOR ¥5500
+
+      The Psyclocks
+      CRACKS
+      SPIKE
+      THE RADIOS
+      THE WOODCHUCK
+      Hi-NOMADY
+      VILLAINS
+      STOMPIN' RIFFRAFFS
+      CHRONOS
+      FREE CHARGE
+      CHICKEN JUMP SKIPS
+      and more
+    `,
+    date: '2026-05-03',
+    image: '/images/2026-05-03.png',
+  },
+  {
+    title: "MAD3 SPECIAL THE NIGHTS",
+    description: `
+      2026.5.15 fri
+      宮古島 GOOD LUCK!
+      OPEN 19:00 / START 19:30
+      Ticket ADV ¥4,000（1Drink Order）
+
+      MAD3
+      O.A. HI-NOMADY
+
+      DJ YOK-jesus
+    `,
+    date: '2026-05-15',
+    image: '/images/2026-05-15.png',
+  },
+  {
+    title: '衝動爆音 vol.7',
+    description: `
+      2026.5.30 sat
+      casquettes presents
+      PLACE: AO'z bar（KANAMACHI）
+      OPEN & START 18:00
+      TICKET: ADV 2,500yen + 1 drink / DOOR 3,000yen + 1 drink
+
+      casquettes
+      爆裂ジャイロ
+      Hi-NOMADY
+      HOBBLEDEES
+
+      DJ's
+      Die-Low（OLDFIRM）
+      DJ Q（東京ロンドン化計画）
+      KAKEI（the WILD ROVER）
+    `,
+    date: '2026-05-30',
+    image: '/images/2026-05-30.png',
+  },
+  {
+    title: '火災救済ライブ Support for Tsubasa & Kanon',
+    description: `
+      2026.5.31 sun
+      club HEAVY SICK
+      Open 17:30 / Start 18:00
+      ¥3,000 + 1drink
+
+      HYDRO STOMPERS
+      Hi-NOMADY
+      THE HIGHMARTS
+
+      DJ
+      Ikb Skate Psychos（D.T.F.S!!! Records）
+      Robinson
+    `,
+    date: '2026-05-31',
+    image: '/images/2026-05-31.png',
+  },
+  {
+    title: 'VILLAINZ × THE JUNGLES!!! SPLIT CD RELEASE PARTY',
+    description: `
+      2026.6.14 sun
+      SHIBUYA 7thFLOOR
+      OPEN 17:30 / START 18:00
+      adv ¥2500 + 1drink / door ¥3000 + 1drink
+
+      VILLAINZ
+      THE JUNGLES!!!
+      Akabane Vulgars on Strong Bypass
+      Hi-NOMADY
+
+      DJ
+      simoneoversound a.k.a. SHIMOKICH!
+      SHIGERU（SLAP of CEMETERY）
+
+      FOOD
+      ササノのごはん
+    `,
+    date: '2026-06-14',
+    image: '/images/2026-06-14.png',
+  },
+  {
+    title: 'TOKYO RUMBLE at TOP BEAT CLUB',
+    description: `
+      2026.6.20 sat
+      荻窪 TOP BEAT CLUB
+      OPEN / START 17:00
+      Ticket ADV 3,500yen / DOOR 4,000yen
+
+      LIVE
+      SPIKE
+      CHICKEN JUMP SKIPS
+      THE PSYCLOCKS
+      Hi-NOMADY
+      BOBBY'S BAR
+
+      DJs
+      RYO THE DYNAMITE
+      SHIGERU
+      DADDY-O-NOV
+      OKITA
+      BU剃井中野
+      Guest DJs
+      SHIDARUMBLE
+      ROBINSON
+      Kartooze
+    `,
+    date: '2026-06-20',
+    image: '/images/2026-06-20.png',
+  },
+  {
+    title: '空襲警報 AIR RAID WARNING',
+    description: `
+      2026.10.11 sun
+      CYBER BOX
+
+      FAT KAZUYA BAND
+      Hi-Nomady
+      ZEROSEN
+      Rudy
+      MONAWA NARES
+    `,
+    date: '2026-10-11',
+    image: '/images/2026-10-11.png',
+  },
 
   // 他のアイテムを追加
 ];
