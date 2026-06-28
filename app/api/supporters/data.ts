e2c6f56776ddd2aec815788d7a641ba7c1d3ef99
Shohei Tanahara <shoheitanahara@Shoheis-MacBook-Air.local>
2026-06-29 00:36:57 +0900

    add video

--- app/api/supporters/data.ts
+++ app/api/supporters/data.ts
@@ -1,5 +1,6 @@
 export const supportersVideos = [
   // 最新の動画を上に追加してください。
+  'https://www.youtube.com/embed/jHp2_Tv9DWc',
   'https://www.youtube.com/embed/AMTYEgeRo9Y',
   'https://www.youtube.com/embed/X_Enw6fLMCw?si=60IeJA8JD64FysZv',
   'https://www.youtube.com/embed/I6OCOB6P2wk',
